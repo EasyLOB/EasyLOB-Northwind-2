f1bbf81e8e6ac7ecbb5c91745bc4f779e26dd9e1
EasyLOB <easylob@gmail.com>
2017-07-08 11:51:26 -0300

    -

---
+++ Northwind.Mvc/EasyLOB/MVC/ValidateModelStateAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Web.Mvc;
+
+namespace EasyLOB.Mvc
+{
+    public class ValidateModelStateAttribute : ActionFilterAttribute
+    {
+        #region Methods
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.Controller.ViewData.ModelState.IsValid == false)
+            {
+                filterContext.Result = new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+        }
+
+        #endregion Methods
+    }
+}
